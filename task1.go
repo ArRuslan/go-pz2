90caab8aa651ad4f522c9662cc5dc62755ab6e4a
RuslanUC <dev_ruslan_uc@protonmail.com>
2025-09-08 12:58:31 +0300

    add matrix part with methods for task1

--- task1.go
+++ task1.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 type IntArr1d []int
+type IntMat2d []IntArr1d
 
 func createAndFillArray1d() IntArr1d {
 	var arrSize uint
@@ -28,6 +29,35 @@ func createAndFillArray1d() IntArr1d {
 	return arr
 }
 
+func createAndFillMat2d() IntMat2d {
+	var matSize uint
+	var err error
+
+	fmt.Printf("Square matrix size: ")
+	_, err = fmt.Scanf("%d", &matSize)
+	if err != nil {
+		panic(err)
+	}
+
+	mat := make(IntMat2d, matSize)
+
+	var i uint
+	for i = 0; i < matSize; i++ {
+		mat[i] = make(IntArr1d, matSize)
+
+		var j uint
+		for j = 0; j < matSize; j++ {
+			fmt.Printf("Element [%d][%d]: ", i, j)
+			_, err = fmt.Scanf("%d", &mat[i][j])
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+
+	return mat
+}
+
 func (r *IntArr1d) sortEven() {
 	size := len(*r)
 
@@ -40,8 +70,29 @@ func (r *IntArr1d) sortEven() {
 	}
 }
 
+func (r *IntMat2d) reverseEven() {
+	size := len(*r)
+	for row := 0; row < size; row += 2 {
+		for i := 0; i < size/2; i++ {
+			(*r)[row][i], (*r)[row][size-i-1] = (*r)[row][size-i-1], (*r)[row][i]
+		}
+	}
+}
+
 func (r *IntArr1d) print() {
 	fmt.Printf("Array: %v\n", r)
+}
+
+func (r *IntMat2d) print() {
+	size := len(*r)
+
+	fmt.Println("Matrix:")
+	for i := 0; i < size; i++ {
+		for j := 0; j < size; j++ {
+			fmt.Printf("%d ", (*r)[i][j])
+		}
+		fmt.Println("")
+	}
 }
 
 /* Введення, виведення вихідних даних та їх обробку здійснити у вигляді функцій. Врахувати ситуації помилок і панік */
@@ -60,4 +111,13 @@ func main() {
 
 	fmt.Printf("Array after sorting: ")
 	arr.print()
+
+	mat := createAndFillMat2d()
+	fmt.Printf("Matrix before reversing: ")
+	mat.print()
+
+	mat.reverseEven()
+
+	fmt.Printf("Matrix after reversing: ")
+	mat.print()
 }
